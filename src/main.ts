8d338d4156d808ede1cd4a749943d52499a761b3
cojack <xcojack@gmail.com>
2020-06-20 22:54:05 +0200

    feat(main): add stats.js to display fps

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from 'three';
+import Stats from 'stats.js';
 
+const stats = new Stats();
 const scene = new Scene();
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+document.body.appendChild(stats.dom);
 
 const geometry = new BoxGeometry();
 const material = new MeshBasicMaterial({color: 0x00ff00});
@@ -21,6 +24,7 @@ function animate() {
 	cube.rotation.y += 0.01;
 
 	renderer.render(scene, camera);
+	stats.update();
 }
 
 animate();
